a14d665b5a2fce2cb173da15c75a5844e60b2949
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-05-24 12:46:21 -0400

    Run prettier

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,18 +1,18 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import reactX from 'eslint-plugin-react-x'
-import reactDom from 'eslint-plugin-react-dom'
-import tseslint from 'typescript-eslint'
-import importX from 'eslint-plugin-import-x'
-import eslintConfigPrettier from 'eslint-config-prettier'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import reactX from "eslint-plugin-react-x";
+import reactDom from "eslint-plugin-react-dom";
+import tseslint from "typescript-eslint";
+import importX from "eslint-plugin-import-x";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.strictTypeChecked,
@@ -21,16 +21,16 @@ export default defineConfig([
       importX.flatConfigs.typescript,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
-      reactX.configs['recommended-typescript'],
+      reactX.configs["recommended-typescript"],
       reactDom.configs.recommended,
       eslintConfigPrettier,
     ],
     languageOptions: {
       globals: globals.browser,
       parserOptions: {
-        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
         tsconfigRootDir: import.meta.dirname,
       },
     },
   },
-])
+]);
