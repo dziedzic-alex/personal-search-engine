5804e1ceffcb8ede8d346f2708919be395947182
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-05-24 12:49:31 -0400

    Fix build issue

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,13 @@ import App from "./App.tsx";
 
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Failed to find the root element");
+}
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
