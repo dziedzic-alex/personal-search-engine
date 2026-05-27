b86674c3a68db8a69942787cbd81ba6c077b81a1
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-05-26 20:19:09 -0400

    Basic creation and persistence of vector embeddings

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -1,9 +1,14 @@
 CREATE EXTENSION vector;
 
+CREATE type document_status AS ENUM ('pending', 'processing', 'completed', 'failed');
+
 CREATE TABLE documents (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
+    status document_status NOT NULL DEFAULT 'pending',
     content_url VARCHAR(255) NOT NULL,
+    content_hash VARCHAR(255) NOT NULL,
+    thumbnail_url VARCHAR(255) NOT NULL DEFAULT '',
     content_type VARCHAR(255) NOT NULL,
     created_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
--- backend/workers/main.py
+++ backend/workers/main.py
@@ -2,9 +2,13 @@ from shared.redis_client import get_redis_client
 from pathlib import Path
 import json
 import fitz
+from sentence_transformers import SentenceTransformer
+from shared.db_client import get_db_client
 
 def main():
     print('Worker is running')
+
+    model = SentenceTransformer("all-MiniLM-L6-v2")
 
     redis_client = get_redis_client()
 
@@ -19,18 +23,36 @@ def main():
 
         print(f"Processing job: {job_data}")
 
-        file_path = Path(job_data['path'])
-        if not file_path.exists():
-            print(f"File not found: {file_path}")
-            continue
+        with get_db_client() as connection:
+            db_document = None
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "SELECT * FROM documents WHERE id = %s", 
+                    (job_data['document_id'],)
+                )
+                db_document = cursor.fetchone()
 
-        document: fitz.Document = fitz.open(file_path)
-        chunks: list[str] = []
-        for page in document:
-            for text_block in page.get_text_blocks():
-                chunks.append(text_block[4].replace("\n", " "))
+            document: fitz.Document = fitz.open(db_document[3])
+            chunks: list[str] = []
+            for page in document:
+                for text_block in page.get_text_blocks():
+                    chunks.append(text_block[4].replace("\n", " "))
 
-        print(f"Chunks: {chunks}")
+            embeddings = model.encode(chunks)
+            with connection.cursor() as cursor:
+                for chunk, embedding in zip(chunks, embeddings):
+                    cursor.execute(
+                        """
+                        INSERT INTO chunks (document_id, content, embedding)
+                        VALUES (%s, %s, %s)
+                        """,
+                        (db_document[0], chunk, embedding)
+                    )
+
+                connection.commit()
+        
+        print(f"Document {db_document[1]} processed")
+
 
 
 if __name__ == "__main__":
