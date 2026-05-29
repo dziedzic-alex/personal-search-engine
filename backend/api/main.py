29555077d0bd97cac7ec0431628d7b34f284a2d9
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-05-28 21:44:03 -0400

    Load models on api process startup so that they can be shared across api endpoints and dont need to be loaded when processing requests at individual endpoints

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
-
+from shared.models.text_embedding import get_text_embedding_model
+from shared.models.image_embedding import get_image_embedding_model
+from shared.redis_client import get_redis_client
 from api.routers import upload
 from api.routers import search
+
+get_text_embedding_model()
+get_image_embedding_model()
+get_redis_client()
 
 app = FastAPI()
 
