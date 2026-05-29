3295f2468ec3a941110fe2d539e82a45aded1db5
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-05-28 21:45:12 -0400

    Redis singleton to be shared per process

--- backend/shared/redis_client.py
+++ backend/shared/redis_client.py
@@ -1,5 +1,10 @@
 import redis
 import os
 
+client: redis.Redis | None = None
+
 def get_redis_client() -> redis.Redis:
-    return redis.Redis.from_url(os.getenv("REDIS_URL"))
+    global client
+    if client is None:
+        client = redis.Redis.from_url(os.getenv("REDIS_URL"))
+    return client
