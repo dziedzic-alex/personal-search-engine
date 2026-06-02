f5f32ff0aac0255bf0267a422176f936f2850c94
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-06-02 17:20:20 -0400

    Register vector extension in database engine so search endpoint can convert search query embeddings to VECTORs

--- backend/db/session.py
+++ backend/db/session.py
@@ -2,9 +2,15 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from collections.abc import Generator
+from sqlalchemy import event
+from pgvector.psycopg import register_vector
 
 engine = create_engine(os.getenv("DATABASE_URL"))
 SessionLocal = sessionmaker(bind=engine)
+
+@event.listens_for(engine, "connect")
+def register_vector_extension(db_connection, connection_record):
+    register_vector(db_connection)
 
 def get_session() -> Generator[Session, None, None]:
     session = SessionLocal()
