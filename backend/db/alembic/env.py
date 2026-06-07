114ecfae3232ecce84cf01d60056269d709117aa
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-06-07 12:38:13 -0400

    Use settings module rather than querying environemnt variables directly

--- backend/db/alembic/env.py
+++ backend/db/alembic/env.py
@@ -1,16 +1,16 @@
-import os
 from logging.config import fileConfig
 
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
 from db.base import Base
+from shared.settings import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
-config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
+config.set_main_option("sqlalchemy.url", settings.database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
