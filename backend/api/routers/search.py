762997251da622ce67c04a69cba989aacbc91133
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-06-12 18:57:37 -0400

    Change api for new search

--- backend/api/routers/search.py
+++ backend/api/routers/search.py
@@ -23,13 +23,12 @@ def search(query: str, search_mode: SearchMode, session: SessionDep):
         relevant_documents = DocumentRepository(session).get_relevant_image_documents(query)
 
     response = []
-    for rrf_score, document, cross_encoding_score in relevant_documents:
+    for document, ranking_score in relevant_documents:
         response.append(
             {
                 "name": document.name,
                 "distance": document.average_distance,
-                "rrf_score": rrf_score,
-                "cross_encoding_score": cross_encoding_score
+                "ranking_score": ranking_score,
             }
         )
 
