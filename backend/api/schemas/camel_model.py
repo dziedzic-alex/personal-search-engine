9046a7b6e53760a673110a90ef9581a1242a78a0
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-06-14 15:04:26 -0400

    Base api request and response model to convert from camelCase frontend provided JSON fields to python snake_case fields

---
+++ backend/api/schemas/camel_model.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel, ConfigDict
+from pydantic.alias_generators import to_camel
+
+# Pydantic model that converts camelCase to snake_case on incoming requests
+# and snake_case to camelCase on outgoing responses
+class CamelModel(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True, from_attributes=True)
