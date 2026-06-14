9ea22bb8d29458adc0ce244209834de44f96cfc7
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-06-14 19:42:56 -0400

    Protected routes for components that require auth to access

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate, Outlet } from "react-router-dom";
+import { use } from "react";
 
 import Navbar from "./Navbar/Navbar.tsx";
 import Home from "./Home/Home.tsx";
@@ -6,20 +7,38 @@ import Upload from "./Upload/Upload.tsx";
 import Search from "./Search/Search.tsx";
 
 import "./App.css";
+import { AuthContext } from "./Auth/AuthContext.tsx";
+import Login from "./Auth/Login.tsx";
+import Signup from "./Auth/Signup.tsx";
 
-function App() {
+function ProtectedRoute() {
+  const { user } = use(AuthContext);
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
   return (
     <>
       <Navbar />
-
       <div className="app-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/upload" element={<Upload />} />
-          <Route path="/search" element={<Search />} />
-        </Routes>
+        <Outlet />
       </div>
     </>
+  );
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/search" element={<Search />} />
+      </Route>
+    </Routes>
   );
 }
 
