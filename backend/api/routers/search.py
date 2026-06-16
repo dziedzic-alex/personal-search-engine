cc0f83d1cd2cadf9a78e8c92c00713ab959aa711
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-06-16 13:16:52 -0400

    Make search route be auth gated

--- backend/api/routers/search.py
+++ backend/api/routers/search.py
@@ -6,10 +6,13 @@ from sqlalchemy.orm import Session
 
 from db.repositories.documents import DocumentRepository
 from db.session import get_session
+from db.models import User
+from api.routers.auth.auth_utils import get_current_user
 
 router = APIRouter(prefix="/search", tags=["search"])
 
 SessionDep = Annotated[Session, Depends(get_session)]
+UserDep = Annotated[User, Depends(get_current_user)]
 
 
 class SearchMode(enum.StrEnum):
@@ -18,14 +21,14 @@ class SearchMode(enum.StrEnum):
 
 
 @router.get("/")
-def search(query: str, search_mode: SearchMode, session: SessionDep):
+def search(query: str, search_mode: SearchMode, session: SessionDep, user: UserDep):
     if search_mode == SearchMode.TEXT:
         relevant_documents = DocumentRepository(session).get_relevant_text_documents(
-            query
+            query, user.id
         )
     elif search_mode == SearchMode.IMAGE:
         relevant_documents = DocumentRepository(session).get_relevant_image_documents(
-            query
+            query, user.id
         )
 
     response = []
