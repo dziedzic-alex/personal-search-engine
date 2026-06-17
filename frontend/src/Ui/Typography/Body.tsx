cde7220a3e78d48895768f3d4e4c91fb6a7c6287
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-06-17 13:45:35 -0400

    Shared body component for general paragraph like text

---
+++ frontend/src/Ui/Typography/Body.tsx
@@ -0,0 +1,14 @@
+import "./Body.css";
+
+interface Props {
+  children: string;
+  variant?: "default" | "muted";
+}
+
+function Body(props: Props) {
+  const { children, variant = "default" } = props;
+
+  return <p className={`body-${variant}`}>{children}</p>;
+}
+
+export default Body;
