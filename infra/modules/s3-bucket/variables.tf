fcddead8aca14ffef1ddd6542da1574b72dcb00a
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-06-21 13:04:48 -0400

    Initial Terraform setup. S3 module to create an S3 bucket and a dev environment which creates an S3 bucket

---
+++ infra/modules/s3-bucket/variables.tf
@@ -0,0 +1,29 @@
+variable "bucket_prefix" {
+    type = string
+    description = "The prefix for the S3 bucket"
+}
+
+variable "region" {
+    type = string
+    description = "The region for the S3 bucket"
+    default = null
+    nullable = true
+}
+
+variable "force_destroy" {
+    type = bool
+    description = "Whether to force destroy the S3 bucket"
+    default = false
+}
+
+variable "environment" {
+    type = string
+    description = "The environment for the S3 bucket"
+}
+
+variable "policy" {
+    type = string
+    description = "The policy for the S3 bucket"
+    default = null
+    nullable = true
+}
