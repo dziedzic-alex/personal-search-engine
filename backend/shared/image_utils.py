c38724c563027a8457156dcb7d51abe6e4867f41
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-06-22 14:00:56 -0400

    Normalize image method shared between upload api and worker during image processing

---
+++ backend/shared/image_utils.py
@@ -0,0 +1,7 @@
+from PIL import Image, ImageOps
+
+def normalize_image(image: Image.Image) -> Image.Image:
+    image = ImageOps.exif_transpose(image)
+    if image.mode not in ["RGB", "L"]:
+        image = image.convert("RGB")
+    return image
