dcfca8a9f08274e729200e5a70bc12f1c95e343a
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-06-22 13:57:09 -0400

    Load singelton s3 client at api start up

--- backend/api/main.py
+++ backend/api/main.py
@@ -7,11 +7,15 @@ from shared.models.cross_encoding import get_cross_encoding_model
 from shared.models.image_embedding import get_image_embedding_model
 from shared.models.text_embedding import get_text_embedding_model
 from shared.redis_client import get_redis_client
+from pillow_heif import register_heif_opener
+from shared.s3_client import get_s3_client
 
 get_text_embedding_model()
 get_image_embedding_model()
 get_cross_encoding_model()
 get_redis_client()
+register_heif_opener()
+get_s3_client()
 
 app = FastAPI()
 
