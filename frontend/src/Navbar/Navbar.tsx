a41e53d6ce13b6803ceed3323177ec19ff19dca4
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-06-22 22:00:03 -0400

    Add notauthed variant to navbar for sign up and log in pages.

--- frontend/src/Navbar/Navbar.tsx
+++ frontend/src/Navbar/Navbar.tsx
@@ -6,7 +6,24 @@ import Navlinks from "./Navlinks.tsx";
 
 import "./Navbar.css";
 
-function Navbar() {
+interface Props {
+  variant?: "authed" | "notauthed";
+}
+
+function Navbar(props: Props) {
+  const { variant = "authed" } = props;
+
+  if (variant === "notauthed") {
+    return (
+      <div className="navbar">
+        <div className="navbar-brand">
+          <LogoIcon className="navbar-logo" />
+          Personal Search
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="navbar">
       <Link to="/" className="navbar-brand">
