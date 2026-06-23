5928a1fc350887cea1ea319eae4c1408225575df
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-06-22 21:55:14 -0400

    Add auth layout to router

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,14 +1,15 @@
 import { createBrowserRouter, Outlet } from "react-router-dom";
 
-import AuthProvider from "./Auth/AuthProvider.tsx";
-import Login from "./Auth/Login.tsx";
-import Signup from "./Auth/Signup.tsx";
-import Files from "./Files/Files.tsx";
-import Home from "./Home/Home.tsx";
-import Profile from "./Profile/Profile.tsx";
-import ProtectedRoute from "./ProtectedRoute.tsx";
-import Search from "./Search/Search.tsx";
-import NotificationProvider from "./Ui/Notification/NotificationProvider.tsx";
+import AuthProvider from "./Auth/AuthProvider";
+import Login from "./Auth/Login";
+import Signup from "./Auth/Signup";
+import AuthLayout from "./AuthLayout";
+import Files from "./Files/Files";
+import Home from "./Home/Home";
+import Profile from "./Profile/Profile";
+import ProtectedRoute from "./ProtectedRoute";
+import Search from "./Search/Search";
+import NotificationProvider from "./Ui/Notification/NotificationProvider";
 
 const router = createBrowserRouter([
   {
@@ -20,8 +21,13 @@ const router = createBrowserRouter([
       </NotificationProvider>
     ),
     children: [
-      { path: "/login", element: <Login /> },
-      { path: "/signup", element: <Signup /> },
+      {
+        element: <AuthLayout />,
+        children: [
+          { path: "/login", element: <Login /> },
+          { path: "/signup", element: <Signup /> },
+        ],
+      },
       {
         element: <ProtectedRoute />,
         children: [
