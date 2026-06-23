08ec75adcd1db629bfe420f94a9c50d96d266fdd
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-06-22 21:55:00 -0400

    Use AppShell component

--- frontend/src/ProtectedRoute.tsx
+++ frontend/src/ProtectedRoute.tsx
@@ -2,24 +2,26 @@ import { Navigate, Outlet } from "react-router-dom";
 
 import { useAuth } from "./Auth/AuthContext.tsx";
 import Navbar from "./Navbar/Navbar.tsx";
+import AppShell from "./Ui/Layout/AppShell.tsx";
 import LoadingPage from "./Ui/LoadingPage/LoadingPage.tsx";
 
 function ProtectedRoute() {
   const { user, isRefreshingAccessToken } = useAuth();
 
   if (!user && isRefreshingAccessToken) {
-    return <LoadingPage />;
+    return (
+      <AppShell>
+        <LoadingPage />
+      </AppShell>
+    );
   } else if (!user && !isRefreshingAccessToken) {
     return <Navigate to="/login" replace />;
   }
 
   return (
-    <>
-      <Navbar />
-      <div className="app-content">
-        <Outlet />
-      </div>
-    </>
+    <AppShell navbar={<Navbar />}>
+      <Outlet />
+    </AppShell>
   );
 }
 
