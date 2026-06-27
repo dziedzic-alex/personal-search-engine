629126aafbe7bc03417dff53fa8421e23c80cb3f
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-06-27 14:33:16 -0400

    Remove uplaod router from fastapi app

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from pillow_heif import register_heif_opener
 
-from api.routers import documents
+from api.routers.documents import router as documents_router
 from api.routers.auth.auth import router as auth_router
-from api.routers.upload.upload import router as upload_router
 from shared.models.cross_encoding import get_cross_encoding_model
 from shared.models.image_embedding import get_image_embedding_model
 from shared.models.text_embedding import get_text_embedding_model
@@ -20,8 +19,7 @@ get_s3_client()
 app = FastAPI()
 
 app.include_router(auth_router)
-app.include_router(upload_router)
-app.include_router(documents.router)
+app.include_router(documents_router)
 
 
 @app.get("/")
