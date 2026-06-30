2eb64249f0bec0c4e0b559e2f03488ac42efac72
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-06-29 20:12:36 -0400

    Dev SQS document processing queue

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -4,3 +4,28 @@ module "files_thumbnails_bucket" {
   environment   = var.environment
   force_destroy = true
 }
+
+module "document_processing_queue" {
+  source = "../modules/sqs-queue"
+  name = "pse-${var.environment}-document-processing"
+  environment = var.environment
+  max_message_size = 2048
+  visibility_timeout_seconds = 180
+  receive_wait_time_seconds = 2
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = module.document_processing_dead_letter_queue.queue_arn
+    maxReceiveCount = 3
+  })
+}
+
+module "document_processing_dead_letter_queue" {
+  source = "../modules/sqs-queue"
+  name = "pse-${var.environment}-document-processing-dead-letter"
+  environment = var.environment
+  max_message_size = 2048
+  message_retention_seconds = 1209600 # 14 days
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue"
+    sourceQueueArns = [module.document_processing_queue.queue_arn]
+  })
+}
--- infra/dev/outputs.tf
+++ infra/dev/outputs.tf
@@ -1,3 +1,7 @@
 output "files_thumbnails_bucket_name" {
   value = module.files_thumbnails_bucket.bucket_name
 }
+
+output "document_processing_queue_name" {
+  value = module.document_processing_queue.queue_name
+}
