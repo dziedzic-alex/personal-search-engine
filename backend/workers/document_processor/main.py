b4054fcf03c36a177e08d83e9fd8be44dc2aa2c7
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-07-01 10:02:26 -0400

    Move message processing logic into internal helper functions

--- backend/workers/document_processor/main.py
+++ backend/workers/document_processor/main.py
@@ -8,7 +8,7 @@ from shared.content_type import IMAGE_CONTENT_TYPE_VALUES, ContentType
 from shared.models.image_embedding import get_image_embedding_model
 from shared.models.text_embedding import get_text_embedding_model
 from shared.s3_client import get_s3_client
-from shared.sqs_client import get_document_processing_sqs_client
+from shared.sqs_client import ConsumerResponse, SQSDocumentProcessingClient, get_document_processing_sqs_client
 from workers.document_processor.image.image import process_image_document
 from workers.document_processor.pdf.pdf import process_pdf_document
 
@@ -28,62 +28,67 @@ def main():
 
     while True:
         try:
-            document_for_processing = sqs_client.get_document_message()
+            document_message = sqs_client.get_document_message()
 
-            if document_for_processing is None:
+            if document_message is None:
                 continue
 
-            print(f"Processing document: {document_for_processing.document_id}")
-
-            with SessionLocal(expire_on_commit=False) as session:
-                document: Document | None = session.get(Document, document_for_processing.document_id)
-
-                if document is None:
-                    print(f"Document {document_for_processing.document_id} not found. Skipping...")
-                    sqs_client.delete_document_message(document_for_processing.receipt_handle)
-                    continue
-
-                if document.status == DocumentStatus.PROCESSED:
-                    print(f"Document {document.name} already processed. Skipping...")
-                    sqs_client.delete_document_message(document_for_processing.receipt_handle)
-                    continue
-                elif document.status != DocumentStatus.PENDING:
-                    print(
-                        f"Document {document.name} is not pending. Skipping..."
-                    )
-                    continue
-
-                document.status = DocumentStatus.PROCESSING
-                session.commit()
-
-            try:
-                if document.content_type in IMAGE_CONTENT_TYPE_VALUES:
-                    process_image_document(document)
-                elif document.content_type == ContentType.PDF.value:
-                    process_pdf_document(document)
-                else:
-                    raise ValueError(f"Unsupported document type: {document.content_type}")
-            except Exception as e:
-                print(f"Error processing document {document.name}: {e}")
-
-                with SessionLocal() as session:
-                    document = session.get(Document, document_for_processing.document_id)
-                    document.status = DocumentStatus.PENDING
-                    session.execute(delete(DocumentEmbedding).where(DocumentEmbedding.document_id == document.id))
-                    session.commit()
-
-                continue
-
-            with SessionLocal(expire_on_commit=False) as session:
-                document = session.get(Document, document_for_processing.document_id)
-                document.status = DocumentStatus.PROCESSED
-                session.commit()
-
-            print(f"Document {document.name} successfully processed")
-            sqs_client.delete_document_message(document_for_processing.receipt_handle)
+            _process_document_message(document_message, sqs_client)
         except Exception as e:
             print(f"Worker error: {e}")
             continue
+
+
+def _process_document_message(document_message: ConsumerResponse, sqs_client: SQSDocumentProcessingClient) -> None:
+    print(f"Processing document: {document_message.document_id}")
+
+    with SessionLocal(expire_on_commit=False) as session:
+        document: Document | None = session.get(Document, document_message.document_id)
+
+        if document is None:
+            print(f"Document {document_message.document_id} not found. Skipping...")
+            sqs_client.delete_document_message(document_message.receipt_handle)
+            return
+
+        if document.status == DocumentStatus.PROCESSED:
+            print(f"Document {document.name} already processed. Skipping...")
+            sqs_client.delete_document_message(document_message.receipt_handle)
+            return
+        elif document.status != DocumentStatus.PENDING:
+            print(
+                f"Document {document.name} is not pending. Skipping..."
+            )
+            return
+
+        document.status = DocumentStatus.PROCESSING
+        session.commit()
+
+    try:
+        if document.content_type in IMAGE_CONTENT_TYPE_VALUES:
+            process_image_document(document)
+        elif document.content_type == ContentType.PDF.value:
+            process_pdf_document(document)
+        else:
+            raise ValueError(f"Unsupported document type: {document.content_type}")
+    except Exception as e:
+        print(f"Error processing document {document.name}: {e}")
+
+        with SessionLocal() as session:
+            document = session.get(Document, document_message.document_id)
+            document.status = DocumentStatus.PENDING
+            session.execute(delete(DocumentEmbedding).where(DocumentEmbedding.document_id == document.id))
+            session.commit()
+
+        return
+
+    with SessionLocal(expire_on_commit=False) as session:
+        document = session.get(Document, document_message.document_id)
+        document.status = DocumentStatus.PROCESSED
+        session.commit()
+
+    print(f"Document {document.name} successfully processed")
+    sqs_client.delete_document_message(document_message.receipt_handle)
+
 
 
 
--- backend/workers/failed_document_watcher/main.py
+++ backend/workers/failed_document_watcher/main.py
@@ -1,4 +1,4 @@
-from shared.sqs_client import get_document_processing_dead_letter_sqs_client
+from shared.sqs_client import ConsumerResponse, SQSDocumentProcessingDeadLetterClient, get_document_processing_dead_letter_sqs_client
 from db.session import SessionLocal
 from db.models.document import Document, DocumentStatus
 
@@ -14,31 +14,34 @@ def main():
             if document_message is None:
                 continue
 
-            with SessionLocal() as session:
-                document = session.get(Document, document_message.document_id)
-
-                if document is None:
-                    print(f"Document {document_message.document_id} not found. Skipping...")
-                    sqs_client.delete_document_message(document_message.receipt_handle)
-                    continue
-
-                if document.status == DocumentStatus.FAILED:
-                    print(f"Document {document_message.document_id} already marked as failed. Skipping...")
-                    sqs_client.delete_document_message(document_message.receipt_handle)
-                    continue
-                elif document.status == DocumentStatus.PROCESSED:
-                    print(f"Document {document_message.document_id} already processed. Skipping...")
-                    sqs_client.delete_document_message(document_message.receipt_handle)
-                    continue
-
-                document.status = DocumentStatus.FAILED
-                session.commit()
-            
-            sqs_client.delete_document_message(document_message.receipt_handle)
+            _process_failed_document_message(document_message, sqs_client)
         except Exception as e:
-            print(f"Error marking document as failed: {e}")
+            print(f"Worker error: {e}")
             continue
 
+
+def _process_failed_document_message(document_message: ConsumerResponse, sqs_client: SQSDocumentProcessingDeadLetterClient) -> None:
+    with SessionLocal() as session:
+        document = session.get(Document, document_message.document_id)
+
+        if document is None:
+            print(f"Document {document_message.document_id} not found. Skipping...")
+            sqs_client.delete_document_message(document_message.receipt_handle)
+            return
+
+        if document.status == DocumentStatus.FAILED:
+            print(f"Document {document_message.document_id} already marked as failed. Skipping...")
+            sqs_client.delete_document_message(document_message.receipt_handle)
+            return
+        elif document.status == DocumentStatus.PROCESSED:
+            print(f"Document {document_message.document_id} already processed. Skipping...")
+            sqs_client.delete_document_message(document_message.receipt_handle)
+            return
+
+        document.status = DocumentStatus.FAILED
+        session.commit()
+    
+    sqs_client.delete_document_message(document_message.receipt_handle)
 
 if __name__ == "__main__":
     main()
