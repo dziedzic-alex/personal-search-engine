6512b38e5e53b5751427cd2ef0e2c57f50b0a3c2
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-07-24 15:54:12 -0400

    Add some environemtn variables for the confirm email flow. ses sending email, region, and frontend base url

--- backend/shared/settings.py
+++ backend/shared/settings.py
@@ -12,6 +12,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
     environment: Environment
+    frontend_base_url: str
     database_url: str
     redis_url: str
     jwt_secret: str
@@ -20,6 +21,8 @@ class Settings(BaseSettings):
     sqs_document_processing_queue_name: str
     sqs_document_processing_dead_letter_queue_name: str
     sqs_document_queues_region: str
+    ses_from_email: str
+    ses_region: str
 
 
 settings = Settings()
