c2ab76ecdf1fe5aaeefea0118e8acab579e0f3b4
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-08-08 12:30:56 -0400

    Shared configure logging function to configure stdlib logger to use rather than print statements for logging

---
+++ backend/shared/configure_logging.py
@@ -0,0 +1,9 @@
+import logging
+
+from shared.settings import Environment, settings
+
+def configure_logging():
+    logging.basicConfig(
+        level=logging.DEBUG if settings.environment == Environment.DEV else logging.INFO,
+        format="%(asctime)s %(levelname)s [%(name)s] %(funcName)s %(message)s",
+    )
