40d2ad4dc4b2f4d18682ca4e6c30634ca825c17e
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-08-20 13:49:39 -0400

    Create vector backed knowledge base in dev env

--- infra/dev/outputs.tf
+++ infra/dev/outputs.tf
@@ -9,3 +9,11 @@ output "document_processing_queue_name" {
 output "document_processing_dead_letter_queue_name" {
   value = module.document_processing_dead_letter_queue.queue_name
 }
+
+output "knowledge_base_id" {
+  value = module.s3_vector_kb.knowledge_base_id
+}
+
+output "knowledge_base_data_source_id" {
+  value = module.s3_vector_kb.knowledge_base_data_source_id
+}
