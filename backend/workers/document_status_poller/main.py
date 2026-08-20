3c5a53c3045a7bf35e0456d868ee51f625c1a0a4
Alex Dziedzic <dziedzicalex182@gmail.com>
2026-08-20 13:56:22 -0400

    Add document status poller to update pending/processing docs status via bedrock api every 30s

---
+++ backend/workers/document_status_poller/main.py
@@ -0,0 +1,58 @@
+from shared.settings import settings
+from shared.configure_logging import configure_logging
+import logging
+from time import sleep
+from shared.bedrock_client import BedrockClient, get_bedrock_client
+from db.session import SessionLocal
+from db.models.document import Document, DocumentStatus
+from sqlalchemy import select, update
+from collections import defaultdict
+import uuid
+
+configure_logging()
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    if not settings.is_document_processing_v2_enabled:
+        logger.info("Document processing v2 is not enabled. Exiting...")
+        exit(0)
+
+    logger.info("Document status poller is running")
+
+    bedrock_client = get_bedrock_client()
+
+    while True:
+        poll_document_statuses(bedrock_client)
+        sleep(30)
+
+def poll_document_statuses(bedrock_client: BedrockClient):
+    try:
+        with SessionLocal() as session:
+            documents_to_poll = session.scalars(select(Document).where(Document.status.in_([DocumentStatus.PENDING, DocumentStatus.PROCESSING]))).all()
+
+        document_ids_to_poll = [document.id for document in documents_to_poll]
+        document_ingestion_statuses = bedrock_client.get_documents_ingestion_statuses(document_ids_to_poll)
+
+        if len(document_ingestion_statuses) == 0:
+            return
+
+        status_to_ids = defaultdict[DocumentStatus, list[uuid.UUID]](list)
+        for ingestion_status in document_ingestion_statuses:
+            if ingestion_status.status == DocumentStatus.PENDING:
+                continue
+            
+            status_to_ids[ingestion_status.status].append(ingestion_status.document_id)
+
+        with SessionLocal() as session:
+            for status, ids in status_to_ids.items():
+                session.execute(update(Document).where(Document.id.in_(ids)).values(status=status))
+
+            session.commit()
+    except Exception:
+        logger.error(f"Error polling & updating document statuses", exc_info=True)
+        pass
+
+if __name__ == "__main__":
+    main()
